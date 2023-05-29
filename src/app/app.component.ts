962324d11e6894ef3e96953db80cddda58bd2d00
VolodymyrIpatenko <81745231+VolodymyrIpatenko@users.noreply.github.com>
2023-05-29 16:27:21 +0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,71 @@
+import { Component, ChangeDetectorRef, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormArray, Validators } from "@angular/forms";
+import { ValidatePassword } from "./must-match/validate-password";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  
+  constructor(
+    public fb: FormBuilder,
+    private cd: ChangeDetectorRef
+  ) {}
+
+  /*##################### Registration Form #####################*/
+  registrationForm = this.fb.group({
+    file: [null]
+  })  
+
+  /*########################## File Upload ########################*/
+  @ViewChild('fileInput') el: ElementRef;
+  imageUrl: any = "https://i.ibb.co/fDWsn3G/buck.jpg";
+  editFile: boolean = true;
+  removeUpload: boolean = false;
+
+  uploadFile(event) {
+    let reader = new FileReader(); // HTML5 FileReader API
+    let file = event.target.files[0];
+    if (event.target.files && event.target.files[0]) {
+      reader.readAsDataURL(file);
+
+      // When file uploads set it to file formcontrol
+      reader.onload = () => {
+        this.imageUrl = reader.result;
+        this.registrationForm.patchValue({
+          file: reader.result
+        });
+        this.editFile = false;
+        this.removeUpload = true;
+      }
+      // ChangeDetectorRef since file is loading outside the zone
+      this.cd.markForCheck();        
+    }
+  }
+
+  // Function to remove uploaded file
+  removeUploadedFile() {
+    let newFileList = Array.from(this.el.nativeElement.files);
+    this.imageUrl = 'https://i.pinimg.com/236x/d6/27/d9/d627d9cda385317de4812a4f7bd922e9--man--iron-man.jpg';
+    this.editFile = true;
+    this.removeUpload = false;
+    this.registrationForm.patchValue({
+      file: [null]
+    });
+  }
+  
+  // Submit Registration Form
+  onSubmit() {
+    this.submitted = true;
+    if(!this.registrationForm.valid) {
+      alert('Please fill all the required fields to create a super hero!')
+      return false;
+    } else {
+      console.log(this.registrationForm.value)
+    }
+  }
+
+}
